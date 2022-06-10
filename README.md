112312558ad10eb64710f328c7d7ab1e3c86cb6d
MoseyByrne <mosey.b.byrne@gmail.com>
2022-06-10 10:13:15 -0700

    WIP create loop that will return a range of numbers up to the inputted number

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
+Describe: returnInput()
+
+Test: "It should return an array of numbers from 0 to the user's inputted number"
+Code: beepBoop(5);
+Expected Output: [0, 1, 2, 3, 4, 5]
+
+
+
+
+
+
+
+
+
+
+
+
 
 # _{Application Name}_
 
--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,21 @@
 //Business Logic
 
-function returnInput(number) {
-  for(let i = 0; i = < number.length; i++) {
+function returnNumberRange(number) {
+  const numberArray = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+  for (let i = 0; i < number.length; i++) {
   console.log('Loop' + i)
   }
+  return console.log;
 }
 
+//UI Logic
 
 $(document).ready(function() {
-  (form#formOne).submit(function(event) {
+  $("form#formOne").submit(function(event) {
     event.preventDefault;
-    $(#results).show();
-    const number =($(#numberInput).val());
-    const result = 
-  
-  })
-})
+    $("#results").show();
+    const number =($("#numberInput").val());
+    const results = returnNumberRange(number);
+    $("#results").append(results + ", ");
+  });
+});
