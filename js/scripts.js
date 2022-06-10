d3a509e4f30adc7472ec1e48504893ef3acf50fe
MoseyByrne <mosey.b.byrne@gmail.com>
2022-06-10 15:33:50 -0700

    pass first TDD

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,26 @@
 //Business Logic
 
 function returnNumberRange(number) {
-  const array = [" 1"," 'Beep'"," 'Boop'"," 4"," 'Won't you be my neighbor?'"," 6"," 7"," 8"," 9"]
+  const array = [0,1,2,3,4,5,6,7,8,9]
   let range = []
   for (let i = 0; i < array.length; i++) {
-    if (i === number-0) break;
-    range.push(array[i]);
+    if (i === number+1) break;
+    range.push(array[i])
+    }
+    return range;
+  };
 
-  }
-  return range;
-}
+
+// 
 
 //UI Logic
 
-$(document).ready(function() {
-  $("form#formOne").submit(function(event) {
-    event.preventDefault();
-    $("#results").show();
-    const number =($("#numberInput").val());
-    const results = returnNumberRange(number);
+// $(document).ready(function() {
+//   $("form#formOne").submit(function(event) {
+//     event.preventDefault();
+//     $("#results").show();
+//     const number =($("#numberInput").val());
+//     const results = returnNumberRange(number);
     
     // if(number === 2) {
     //   $("#results").text('"Beep!"') 
@@ -27,10 +29,10 @@ $(document).ready(function() {
     // } else if(number == 5){
     //   $("#results").text('"Wont you be my neighbor?"')
     // } else {
-      $("#results").text(results);
+      // $("#results").text(results);
     
       
     
 
-  });
-});
+//   });
+// });
