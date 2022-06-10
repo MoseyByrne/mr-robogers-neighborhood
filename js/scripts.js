efdf2457bb3479bad0679063e479528e263267a9
MoseyByrne <mosey.b.byrne@gmail.com>
2022-06-10 09:26:33 -0700

    WIP create loop that will return a range of numbers up to the inputted number

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+//Business Logic
+
+function returnInput(number) {
+  for(let i = 0; i = < number.length; i++) {
+  console.log('Loop' + i)
+  }
+}
+
+
+$(document).ready(function() {
+  (form#formOne).submit(function(event) {
+    event.preventDefault;
+    $(#results).show();
+    const number =($(#numberInput).val());
+    const result = 
+  
+  })
+})
